d03eb7900112176383b288e5b8bc5eaaa989d1a5
chic-page-6502 <zacharytq@gmail.com>
2019-07-10 22:49:27 +0000

    Automatically backed up by Learn

--- lib/turn.rb
+++ lib/turn.rb
@@ -15,3 +15,22 @@ end
 def move(board, position, char = "X")
   board[position] = char
 end
+
+def position_taken?(board, index)
+  if board[index] == " " || board[index] == "" || board[index] == nil || board[index] == "  "
+    false
+  elsif board[index] == "X" || board[index] == "O"
+    true
+  end
+end
+
+def valid_move?(board, index)
+  if index > 8
+    false
+  elsif position_taken?(board, index)
+    false
+  else
+    true
+  end
+
+end
